cbbc2058348b8b95a2179258ae6fb86e62197cc1
hal90 <norrisj0612@gmail.com>
2021-08-01 09:16:45 -0400

    Added repeated test.

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -33,5 +35,12 @@ class PersonTest implements ModelTests {
         assertAll("Testing Properties are set...",
                 () -> assertEquals( "Joe", person.getFirstName(), "First name failed..."),
                 () -> assertEquals( "Shmoe", person.getLastName(),"Last name failed..."));
+    }
+
+    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} of {totalRepetitions}")
+    @DisplayName("My Repeated Test")
+    @Test
+    void myRepeatedTest() {
+        // TODO - impl
     }
 }
