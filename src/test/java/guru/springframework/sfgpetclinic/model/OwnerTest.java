dd2bcf10fc69f55f0505072f685e6608b26940ea
hal90 <norrisj0612@gmail.com>
2021-08-16 08:56:17 -0400

    Added csvFileSource.

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -65,6 +66,13 @@ class OwnerTest implements ModelTest {
             "NM, 3, 3"
     })
     void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " : " + val1 + " : " + val2);
+    }
+
+    @DisplayName("CSV From File Test")
+    @ParameterizedTest(name = "{displayName} - {index}] {arguments}")
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvFromFileTest(String stateName, int val1, int val2) {
         System.out.println(stateName + " : " + val1 + " : " + val2);
     }
 
